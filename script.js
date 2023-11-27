d33453c1a877c6960e630f2a21d0fe340ac2c143
Amit kumar <88128579+aamitttt@users.noreply.github.com>
2023-11-27 14:47:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-
+const API_KEY="your key"
 const chatbotToggler = document.querySelector(".chatbot-toggler");
 const closeBtn = document.querySelector(".close-btn");
 const chatbox = document.querySelector(".chatbox");
